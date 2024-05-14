0e0a0a9f9a9d92e034ce1fa67c761ca4b155dfe4
matanhaz <48633383+matanhaz@users.noreply.github.com>
2024-05-14 18:26:16 +0300

    Update extract_projects.sh

--- extract_projects.sh
+++ extract_projects.sh
@@ -5,7 +5,9 @@ OUTPUT=$3
 # checkout and get repo folder
 mkdir  "${OUTPUT}/${PROJECT}"
 mkdir  "${OUTPUT}/${PROJECT}/${VERSION}"
+echo 1
 defects4j checkout -p "${PROJECT}" -v "${VERSION}b" -w "${OUTPUT}/${PROJECT}/${VERSION}"
+echo 2
 
 
 # extract all components
@@ -14,13 +16,17 @@ OUTPUT_PATH="${DIRECTORY}/all_components.txt"
 INPUT_PATH="${DIRECTORY}/repo/src"
 
 java -jar extractClass.jar -i $INPUT_PATH -o $OUTPUT_PATH
+echo 3
 
 # extract tests
 ./extract_tests.sh $PROJECT $VERSION $OUTPUT $OUTPUT
+echo 4
 
 # extracting ground truth
 python ./ground_truth.py $PROJECT $VERSION
 cp ground_truth/$PROJECT/$VERSION/ground_truth.txt projects/$PROJECT/$VERSION
+echo 5
 
 # extracting traces
 ./extract_coverage.sh $PROJECT $VERSION $OUTPUT $OUTPUT
+echo 6
